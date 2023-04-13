e6f881b32f61d76ee8c0caa70c915b13e23d13ed
Vanessa O <vaneolipa@gmail.com>
2023-04-12 23:28:43 -0500

    Fix linters errors

--- person.rb
+++ person.rb
@@ -1,29 +1,24 @@
 class Person
-  def initialize(name = "Unknown", age, parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   attr_reader :id
 
-  attr_accessor :name
-
-  attr_accessor :age
+  attr_accessor :name, :age
 
   def can_use_services
-    if @age >= 18 && parent_permission
-      true
-    end
+    return unless @age >= 18 && parent_permission
+
+    true
   end
 
   private
 
   def of_age
-    if @age >= 18
-      true      
-    else
-      false
-    end
+    @age >= 18
   end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,9 @@
-require "./person.rb"
+require './person'
 
 class Teacher < Person
-  def initialize(specialization, name = "Unknown", age, parent_permission: true)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission: parent_permission)
-    @specialization = specialization   
+    @specialization = specialization
   end
 
   def can_use_services
