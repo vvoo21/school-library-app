f201c8e7cf5ff8e9c6af96cfb5b3580f21acf7ee
Vanessa O <vaneolipa@gmail.com>
2023-04-12 22:18:55 -0500

    Create class Person

---
+++ person.rb
@@ -0,0 +1,29 @@
+class Person
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+  end
+
+  attr_reader :id
+
+  attr_accessor :name
+
+  attr_accessor :age
+
+  def can_use_services
+    if @age >= 18 && parent_permission
+      true
+    end
+  end
+
+  private
+
+  def of_age
+    if @age >= 18
+      true      
+    else
+      false
+    end
+  end
+end
