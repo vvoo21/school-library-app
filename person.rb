b708e1b7e32425068eaf20eb47892e1e9e41c608
Vanessa O <vaneolipa@gmail.com>
2023-04-13 17:47:05 -0500

    Person class inherits from Nameable and Add correct_name method

--- person.rb
+++ person.rb
@@ -1,5 +1,8 @@
-class Person
+require "./nameable.rb"
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -16,6 +19,11 @@ class Person
     true
   end
 
+  def correct_name
+    @name
+  end
+
+  # Private methods
   private
 
   def of_age
