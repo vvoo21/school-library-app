10cbe46740c24f8c0cdb13043f9e6a9fe6b7fe93
Vanessa O <vaneolipa@gmail.com>
2023-04-14 18:38:27 -0500

    Create relationship Person/Book.

--- person.rb
+++ person.rb
@@ -1,17 +1,22 @@
 require './nameable'
 
 class Person < Nameable
+  attr_reader :id, :rentals
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
-  attr_reader :id
-
-  attr_accessor :name, :age
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
+  end
 
   def can_use_services
     return unless @age >= 18 && parent_permission
