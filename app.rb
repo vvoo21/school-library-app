3afd7d88be7b2fe284e181af0d988dfd0dca5d9c
Vanessa O <vaneolipa@gmail.com>
2023-04-15 18:39:24 -0500

    Create methods: options, choose_an_option, and start

---
+++ app.rb
@@ -0,0 +1,55 @@
+require './book'
+require './student'
+require './teacher'
+require './rental'
+
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+  def options
+    puts 'Please choose an option by enterin a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def choose_an_option(choice)
+    case choice
+    when 1
+      list_books
+    when 2
+      list_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rentals
+    when 7
+      puts 'Thank you for using this app!'
+    else
+      puts 'Invalid choice.'
+      puts ''
+      start
+    end
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
+
+  def start
+    options
+    choice = gets.chomp.to_i
+    choose_an_option(choice)
+  end
+
+end
